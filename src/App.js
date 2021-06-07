148cedd45a21a2be61095a25775783ddfc3365c3
joydey <joydey02@gmail.com>
2021-06-07 19:30:34 +0530

    added home and contacts page and header component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import ShopPage from './components/shop/shop.component';
+import Header from './components/header/header.component';
 
 
 const HatsPage = () =>(
@@ -14,9 +15,12 @@ const HatsPage = () =>(
 function App() {
   return (
     <div className="App">
+      <Header/>
+      <Switch>
      <Route exact path='/' component={HomePage} />
      <Route  exact path='/hats' component={HatsPage} />
      <Route path="/shop" component={ShopPage}/>
+     </Switch>
     </div>
   );
 }
--- src/components/shop/shop.component.jsx
+++ src/components/shop/shop.component.jsx
@@ -3,14 +3,17 @@ import CollectionPreview from '../Collection-preview/Collection-Preview.Componen
 import SHOP_DATA from './shop.data';
 
 class ShopPage extends React.Component{
-    constructor(props){
+    constructor(props)
+    {
         super(props);
-        this.state= {
-            collections: SHOP_DATA
+        this.state= {collections: SHOP_DATA
         }
+    
     }
     render(){
+
     const {collections} = this.state;
+    
     return(
     <div className="shop-page">
         {
